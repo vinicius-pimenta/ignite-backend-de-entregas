66503eb92338b100b5a15e492595caa4aac2a592
Vinicius <viniciustavarespimenta@gmail.com>
2021-12-30 08:56:15 -0300

    feat: create route authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { CreateClientController } from './modules/clients/use-cases/create-client/create-client-controller'
+import { AuthenticateClientController } from './modules/account/authenticate-client/authenticate-client-controller'
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
+const authenticateClientController = new AuthenticateClientController()
 
+routes.post('/authenticate', authenticateClientController.handle)
 routes.post('/client/', createClientController.handle)
 
 export { routes }
