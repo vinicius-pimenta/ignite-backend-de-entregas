f1ea53dc3a7b9b615ef0d389bc5c5d511dc704c3
Vinicius <viniciustavarespimenta@gmail.com>
2021-12-30 10:37:13 -0300

    feat: create delivery route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from './modules/clients/use-cases/create-clien
 import { AuthenticateClientController } from './modules/account/authenticate-client/authenticate-client-controller'
 import { CreateDeliverymanController } from './modules/deliveryman/use-cases/create-deliveryman/create-deliveryman-controller'
 import { AuthenticateDeliverymanController } from './modules/account/authenticate-deliveryman/authenticate-deliveryman-controller'
+import { CreateDeliveryController } from './modules/deliveries/use-cases/create-delivery/create-delivery-controller'
 
 const routes = Router()
 
@@ -11,10 +12,14 @@ const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 
+const deliveryController = new CreateDeliveryController()
+
 routes.post('/client/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
 routes.post('/client/', createClientController.handle)
 routes.post('/deliveryman', createDeliverymanController.handle)
+
+routes.post('/delivery', deliveryController.handle)
 
 export { routes }
